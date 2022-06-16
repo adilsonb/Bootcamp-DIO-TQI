8be0f728eac560764d7db3fbaf1ad334bcf1794a
Adilson Borszcz <adilsonb@users.noreply.github.com>
2022-06-16 19:34:08 -0300

    Initial Commit

---
+++ Modulo-VI-Java/Dominando-IDEs-Java/calculadora-java/src/br/com/calculadora/Calculadora.java
@@ -0,0 +1,46 @@
+package br.com.calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		try (Scanner scan = new Scanner(System.in)) {
+			int a, b;
+			
+			System.out.println("Digite o primeiro valor: ");
+			a = scan.nextInt();
+			System.out.println("Digite o segundo valor: ");
+			b = scan.nextInt();
+			
+			int soma = soma(a,b);
+			int subtracao = subtracao(a,b);
+			int multiplicacao = multiplicacao(a,b);
+			float divisao = divisao(a,b);
+			
+			System.out.println("A soma de " + a + " + " + b + " é: " + soma);
+			System.out.println("A subtracao de " + a + " - " + b + " é: " +  subtracao);
+			System.out.println("A multiplicação de " + a + " x " + b + " é: " +  multiplicacao);
+			System.out.println("A divisão de " + a + " / " + b + " é: " + divisao);
+		}
+		
+		
+	}
+
+	public static int soma(int a, int b) {
+		return a + b;
+	}
+
+	public static int subtracao(int a, int b) {
+		return a - b;
+	}
+
+	public static float divisao(float a, float b) {
+		return a / b;
+	}
+
+	public static int multiplicacao(int a, int b) {
+		return a * b;
+	}
+
+}
